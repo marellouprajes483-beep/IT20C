34670d9a9e3fb12f0fab796989635a37061d3bcb
ellaiza19 <ellaizadajang22@gmail.com>
2025-11-20 14:51:57 +0800

    Added do-while-loop

---
+++ IT20C/loopings.js
@@ -0,0 +1,29 @@
+// for loop
+
+for(i =1; i<=5 ;i++){
+    console.log(i);
+    output +=i +" ";
+}
+
+//console.log (output);
+
+
+//array
+output =" ";
+arr = [1,2,3]
+
+for (index =0; index <arr.length -1; indexi++){
+    //console.log(arr[i]);
+    output += arr[index] + " ";
+}
+console.log(output);
+
+output =" ";
+arr = [1,2,3]
+console.log ("Array length; "+ arr.length);
+
+for (index =arr.length -1;index>=0; index--){
+    //console.log(arr[i]);
+    output += arr[index] + " ";
+}
+console.log(output);
---
+++ IT20C/while-loop.js
@@ -0,0 +1,38 @@
+console.log("reverse");
+
+i = 5;
+
+while (i <= 5){
+    console.log(i);
+    i++;
+}
+
+ console.log("ascending");
+x = 1;
+
+while (x >= 5){
+    console.log(x);
+    i++;
+}
+
+
+output =" ";
+x=1;
+
+while (x<-5){
+    ooutput += x + " ";
+    x++;
+}
+
+console.log (output);
+
+arr=[4,2,3];
+output += x+" ";
+index = 1;
+
+while(index <= arr.length){
+    output += index + " ";
+    index =0
+}
+
+console.log(output);
